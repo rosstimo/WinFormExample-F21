168f9d4805fdb27010da95c4d6d0b9761b1c9499
rosstimo <rosstimo@isu.edu>
2021-09-28 08:50:44 -0600

    Add project files.

---
+++ WinFormExample-F21/WinFormExampleForm.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class WinFormExampleForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.HelloButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.UserInputTextBox = New System.Windows.Forms.TextBox()
+        Me.UserInputLabel = New System.Windows.Forms.Label()
+        Me.UpdateButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'HelloButton
+        '
+        Me.HelloButton.Location = New System.Drawing.Point(200, 165)
+        Me.HelloButton.Name = "HelloButton"
+        Me.HelloButton.Size = New System.Drawing.Size(165, 103)
+        Me.HelloButton.TabIndex = 2
+        Me.HelloButton.Text = "Say &Hello"
+        Me.HelloButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(371, 165)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(165, 103)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'UserInputTextBox
+        '
+        Me.UserInputTextBox.Location = New System.Drawing.Point(49, 6)
+        Me.UserInputTextBox.Name = "UserInputTextBox"
+        Me.UserInputTextBox.Size = New System.Drawing.Size(291, 20)
+        Me.UserInputTextBox.TabIndex = 0
+        '
+        'UserInputLabel
+        '
+        Me.UserInputLabel.AutoSize = True
+        Me.UserInputLabel.Location = New System.Drawing.Point(12, 9)
+        Me.UserInputLabel.Name = "UserInputLabel"
+        Me.UserInputLabel.Size = New System.Drawing.Size(31, 13)
+        Me.UserInputLabel.TabIndex = 3
+        Me.UserInputLabel.Text = "Input"
+        '
+        'UpdateButton
+        '
+        Me.UpdateButton.Location = New System.Drawing.Point(29, 165)
+        Me.UpdateButton.Name = "UpdateButton"
+        Me.UpdateButton.Size = New System.Drawing.Size(165, 103)
+        Me.UpdateButton.TabIndex = 1
+        Me.UpdateButton.Text = "&Update"
+        Me.UpdateButton.UseVisualStyleBackColor = True
+        '
+        'WinFormExampleForm
+        '
+        Me.AcceptButton = Me.UpdateButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(548, 280)
+        Me.Controls.Add(Me.UpdateButton)
+        Me.Controls.Add(Me.UserInputLabel)
+        Me.Controls.Add(Me.UserInputTextBox)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.HelloButton)
+        Me.Name = "WinFormExampleForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form Example"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents HelloButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents UserInputTextBox As TextBox
+    Friend WithEvents UserInputLabel As Label
+    Friend WithEvents UpdateButton As Button
+End Class
---
+++ WinFormExample-F21/WinFormExampleForm.vb
@@ -0,0 +1,15 @@
+﻿Public Class WinFormExampleForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub HelloButton_Click(sender As Object, e As EventArgs) Handles HelloButton.Click
+        'MsgBox("Hello User")
+        Me.Text = "Super Duper"
+        HelloButton.Text = "Pressed"
+    End Sub
+
+    Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
+        Me.Text = UserInputTextBox.Text
+    End Sub
+End Class
